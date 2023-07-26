bf9f30abac8dbb47345fe72408f649ff19dff5d6
Vansh Sharma <113132865+vansh9421@users.noreply.github.com>
2023-07-27 03:04:06 +0530

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let page=1;
 
 async function searchImages(){
     inputData=inputEl.value;
-    const url= 'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}';
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response= await fetch(url);
     const data = await response.json();
@@ -34,8 +34,9 @@ async function searchImages(){
 
         imageWrapper.appendChild(image);
         imageWrapper.appendChild(imageLink);
-        imageWrapper.appendChild(imageWrapper);
+        searchResults.append(imageWrapper);
 
+        
     });
 
     page++;
